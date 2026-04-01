f23e5719831c11b42fe918ea9e1fea3e8a8d0b17
211RDC039 <konradskonrads3@gmail.com>
2026-04-01 21:21:43 +0300

    chore: initial commit, setup static build and include out folder

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
